69e49b51a3e4e50c106239789822ef16b2dacf4b
Pamela Fox <pamela.fox@gmail.com>
2023-07-28 16:41:56 -0700

    More pix

--- automate-a11y/test_axe_count.py
+++ automate-a11y/test_axe_count.py
@@ -2,6 +2,6 @@ from axe_playwright_python.sync_playwright import Axe
 from playwright.sync_api import Page
 
 def test_a11y(page: Page):
-    page.goto("https://2023.northbaypython.org/")
+    page.goto("https://www.pamelafox.org")
     results = Axe().run(page)
     assert results.violations_count == 0, results.generate_report()
